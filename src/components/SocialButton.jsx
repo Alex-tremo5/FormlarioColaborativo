f8e4ab0a93fd6ec1a64b4434486db1f5cbdc8a61
Chokis <mapaulaquintero@gmail.com>
2024-03-30 12:03:14 -0500

    add social web icons

--- src/components/SocialButton.jsx
+++ src/components/SocialButton.jsx
@@ -3,7 +3,7 @@ import Icon from "./Icon"
 const SocialButton = () => {
     return (
         <div className="social">
-            <button className="border"><Icon icon={faFacebook} size="2x"/></button>
+            <button style={{borderColor:"black"}} className="border"><Icon icon={faFacebook} size="2x"/></button>
             <button className="border"><Icon icon={faGithub} size="2x"/></button>
             <button className="border"><Icon icon={faLinkedinIn} size="2x"/></button>
         </div>
