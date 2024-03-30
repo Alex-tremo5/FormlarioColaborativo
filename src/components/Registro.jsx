c2caa240d3149925598b9df399f6b81449463d00
Alejandro <alejandroidb@gmail.com>
2024-03-29 23:01:06 -0300

    creacion componente registro

---
+++ src/components/Registro.jsx
@@ -0,0 +1,10 @@
+const Registro = () => {
+    return (
+        <div className="registro pt-2 pb-2 mt-2 ">
+            <h1>Crea tu cuenta</h1>
+            <SocialButton />
+            <Formulario />
+        </div>
+    )
+}
+export default Registro
